dfd327a57e4c9e07e6334a55e19d4b156cfbcd0b
Araz Maharramli <araz.maharramli@gmail.com>
2021-02-13 01:35:47 +0400

    slug generator and file services created

---
+++ ShoppingApp.Services/FileServices/FileUploader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ShoppingApp.Services.FileServices
+{
+    public class FileService : IFileService
+    {
+        public bool DeleteFile(string path)
+        {
+            try
+            {
+                File.Delete(path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file, string root, string folder, string fileName)
+        {
+            if (file != null)
+            {
+                string uniqueFileName = null;
+                string uploadsFolder = Path.Combine(root, "uploads", folder);
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+                var extension = file.FileName.Substring(file.FileName.LastIndexOf(".", StringComparison.Ordinal)).ToLower();
+                uniqueFileName = Guid.NewGuid().ToString("N") + "_" + fileName + extension;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+                return Path.Combine("uploads", folder, uniqueFileName);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ShoppingApp.Services/FileServices/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ShoppingApp.Services.FileServices
+{
+    public interface IFileService
+    {
+        public Task<string> UploadFileAsync(IFormFile file, string folder, string name, string fileNameType = "CompanyRegistrationCertificate");
+        public bool DeleteFile(string path);
+    }
+}
+
